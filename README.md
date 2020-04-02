b4e575d38915b634f4aeda8c038c2c94e34357eb
Steve Day <steve@openownership.org>
2020-04-02 15:54:32 +0100

    Save to GDrive, not git

--- README.md
+++ README.md
@@ -1,17 +1,13 @@
 # Notion Backup
 
-This repo stores a backup of [OpenOwnership's Notion workspace](https://www.notion.so/openownership/OpenOwnership-Home-e6298eada76f4535bc2ea177ba79413e)
-and the code which makes that backup and regularly updates it.
-
-The backup itself is in [/backup](https://github.com/openownership/notion-backup/tree/master/backup)
-this is a copy of Notion's 'workspace export' in the 'markdown + csv' format.
+This repo makes a backup of OpenOwnership's Notion workspace.
 
 The code runs via a Github Action, which is scheduled to run at 4:05am every
 day, as well as whenever code is pushed to the repository.
 
 ## Running the code locally
 
-The code to run the backup lives in [/notion/export_notion.py]. To run it:
+The code to run the backup lives in /notion/export_notion.py. To run it:
 
 1. Install requirements
 
@@ -24,13 +20,14 @@ The code to run the backup lives in [/notion/export_notion.py]. To run it:
    touch .env
    ```
 
-2. Set your `NOTION_SPACE_ID`, `NOTION_EMAIL` and `NOTION_PASSWORD` in the
-   `.env` file:
+2. Set your credentials in the `.env` file:
 
    ```shell
    NOTION_SPACE_ID=1234-56789-abcdef
    NOTION_EMAIL=notion@example.com
    NOTION_PASSWORD=password
+   GDRIVE_ROOT_FOLDER_ID=<get-the-folder-id-from-gdrive>
+   GDRIVE_SERVICE_ACCOUNT=<get-the-service-account-info-from-1password>
    ```
 
 3. Run the python module: `python notion`
@@ -44,6 +41,5 @@ Google SSO.
 
 ## Github Action config
 
-The Github Action is configured via `NOTION_EMAIL`, `NOTION_PASSWORD` and
-`NOTION_SPACE_ID` secrets set up in [the repo settings](https://github.com/openownership/notion-backup/settings/secrets).
+The Github Action is configured via secrets set up in [the repo settings](https://github.com/openownership/notion-backup/settings/secrets).
 These are then set as env vars for the python script to use.
--- notion/export_notion.py
+++ notion/export_notion.py
@@ -1,12 +1,14 @@
 import os
 import shutil
-from zipfile import ZipFile
 import time
-from pathlib import Path
+import json
+import datetime
 
 import requests
 from notion.client import NotionClient
 from dotenv import load_dotenv
+from google.oauth2 import service_account
+from google.auth.transport.requests import AuthorizedSession
 
 
 load_dotenv()
@@ -21,7 +23,11 @@ def notionToken():
         # Notion obviously check this as some kind of (bad) test of CSRF
         'host': 'www.notion.so'
     }
-    response = requests.post('https://notion.so/api/v3/loginWithEmail', json=loginData, headers=headers)
+    response = requests.post(
+        'https://notion.so/api/v3/loginWithEmail',
+        json=loginData,
+        headers=headers
+    )
     response.raise_for_status()
     return response.cookies['token_v2']
 
@@ -42,12 +48,12 @@ def exportTask():
     }
 
 
-
 def exportUrl(client, taskId):
     url = False
     print('Polling for export task: {}'.format(taskId))
     while True:
-        tasks = client.post('getTasks', {'taskIds': [taskId]}).json().get('results')
+        response = client.post('getTasks', {'taskIds': [taskId]})
+        tasks = response.json().get('results')
         task = next(t for t in tasks if t['id'] == taskId)
         if task['state'] == 'success':
             url = task['status']['exportURL']
@@ -60,16 +66,41 @@ def exportUrl(client, taskId):
     return url
 
 
-
 def downloadFile(url, filename):
     with requests.get(url, stream=True) as r:
-        with open(filename, 'wb') as f:
+        with open('backup/{}'.format(filename), 'wb') as f:
             shutil.copyfileobj(r.raw, f)
 
 
-def extractFile(filename, folder):
-    ZipFile(filename).extractall(folder)
-    os.remove(filename)
+def createFolder(session):
+    metadata = {
+        'name': datetime.datetime.utcnow().isoformat(),
+        'mimeType': 'application/vnd.google-apps.folder',
+        'parents': [os.getenv('GDRIVE_ROOT_FOLDER_ID')]
+    }
+    response = session.post(
+        'https://www.googleapis.com/drive/v3/files?supportsAllDrives=true',
+        json=metadata
+    )
+    response.raise_for_status()
+    return response.json()['id']
+
+
+def upload(name, session, folderId, mimeType):
+    file_metadata = {
+        'name': name,
+        'mimeType': mimeType,
+        'parents': [folderId]
+    }
+    start_response = session.post(
+        'https://www.googleapis.com/upload/drive/v3/files?uploadType=resumable&supportsAllDrives=true',
+        json=file_metadata
+    )
+    start_response.raise_for_status()
+    resumable_uri = start_response.headers.get('Location')
+    file = open('backup/{}'.format(name), 'rb')
+    upload_response = session.put(resumable_uri, data=file)
+    upload_response.raise_for_status()
 
 
 def main():
@@ -78,7 +109,15 @@ def main():
     taskId = client.post('enqueueTask', exportTask()).json().get('taskId')
     url = exportUrl(client, taskId)
     downloadFile(url, 'export.zip')
-    extractFile('export.zip', 'backup')
+
+    service_account_info = json.loads(os.getenv('GDRIVE_SERVICE_ACCOUNT'))
+    google_credentials = service_account.Credentials.from_service_account_info(
+        service_account_info,
+        scopes=['https://www.googleapis.com/auth/drive']
+    )
+    authed_session = AuthorizedSession(google_credentials)
+    folderId = createFolder(authed_session)
+    upload('export.zip', authed_session, folderId, 'application/zip')
 
 
 if __name__ == "__main__":
